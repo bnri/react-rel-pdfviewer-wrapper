232702376fe76614da7ee37a2e8c7140ed2c8e99
guripong <guripong@hanmail.net>
2023-06-23 18:59:18 +0900

    fix

---
+++ dist/svg/RemoconSVG.js
@@ -0,0 +1,70 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var RemoconSVG = function RemoconSVG() {
+  return /*#__PURE__*/_react["default"].createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    xmlnsXlink: "http://www.w3.org/1999/xlink",
+    version: "1.1",
+    width: "256",
+    height: "256",
+    viewBox: "0 0 256 256",
+    xmlSpace: "preserve"
+  }, /*#__PURE__*/_react["default"].createElement("g", {
+    style: {
+      stroke: 'none',
+      strokeWidth: 1,
+      strokeDasharray: 'none',
+      strokeLinecap: 'butt',
+      strokeLinejoin: 'miter',
+      strokeMiterlimit: 10,
+      fill: 'rgb(0,0,0)',
+      fillRule: 'nonzero',
+      opacity: 1
+    },
+    transform: "translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)"
+  }, /*#__PURE__*/_react["default"].createElement("path", {
+    d: "M 60.103 90 H 29.897 c -4.525 0 -8.207 -3.682 -8.207 -8.207 V 8.207 C 21.69 3.682 25.372 0 29.897 0 h 30.205 c 4.525 0 8.207 3.682 8.207 8.207 v 73.586 C 68.31 86.318 64.628 90 60.103 90 z M 29.897 4 c -2.32 0 -4.207 1.887 -4.207 4.207 v 73.586 c 0 2.319 1.887 4.207 4.207 4.207 h 30.205 c 2.319 0 4.207 -1.888 4.207 -4.207 V 8.207 C 64.31 5.887 62.422 4 60.103 4 H 29.897 z",
+    transform: " matrix(1 0 0 1 0 0) "
+  }), /*#__PURE__*/_react["default"].createElement("path", {
+    d: "M 37.233 72.872 c -0.512 0 -1.024 -0.195 -1.414 -0.586 l -3.823 -3.823 c -0.375 -0.375 -0.585 -0.884 -0.585 -1.414 s 0.211 -1.039 0.586 -1.414 l 3.823 -3.822 c 0.781 -0.781 2.048 -0.781 2.828 0 c 0.781 0.781 0.781 2.048 0 2.828 l -2.408 2.408 l 2.408 2.409 c 0.781 0.781 0.781 2.048 0 2.828 C 38.256 72.677 37.745 72.872 37.233 72.872 z",
+    transform: " matrix(1 0 0 1 0 0) "
+  }), /*#__PURE__*/_react["default"].createElement("path", {
+    d: "M 41.177 61.281 c -0.512 0 -1.024 -0.195 -1.414 -0.586 c -0.781 -0.781 -0.781 -2.048 0 -2.828 l 3.823 -3.822 c 0.781 -0.781 2.047 -0.781 2.828 0 l 3.822 3.822 c 0.781 0.781 0.781 2.047 0 2.828 s -2.047 0.781 -2.828 0 L 45 58.287 l -2.409 2.408 C 42.201 61.086 41.689 61.281 41.177 61.281 z",
+    transform: " matrix(1 0 0 1 0 0) "
+  }), /*#__PURE__*/_react["default"].createElement("path", {
+    d: "M 52.768 72.872 c -0.512 0 -1.023 -0.195 -1.414 -0.586 c -0.781 -0.78 -0.781 -2.047 0 -2.828 l 2.408 -2.409 l -2.408 -2.408 c -0.781 -0.781 -0.781 -2.047 0 -2.828 s 2.047 -0.781 2.828 0 l 3.822 3.822 c 0.781 0.781 0.781 2.047 0 2.828 l -3.822 3.823 C 53.791 72.677 53.279 72.872 52.768 72.872 z",
+    transform: " matrix(1 0 0 1 0 0) "
+  }), /*#__PURE__*/_react["default"].createElement("path", {
+    d: "M 45 80.639 c -0.512 0 -1.023 -0.195 -1.414 -0.586 l -3.823 -3.822 c -0.781 -0.78 -0.781 -2.047 0 -2.828 c 0.78 -0.781 2.047 -0.781 2.828 0 L 45 75.811 l 2.408 -2.408 c 0.781 -0.781 2.047 -0.781 2.828 0 s 0.781 2.047 0 2.828 l -3.822 3.822 C 46.023 80.443 45.512 80.639 45 80.639 z",
+    transform: " matrix(1 0 0 1 0 0) "
+  }), /*#__PURE__*/_react["default"].createElement("path", {
+    d: "M 47.791 69.049 h -5.582 c -1.104 0 -2 -0.896 -2 -2 s 0.896 -2 2 -2 h 5.582 c 1.104 0 2 0.896 2 2 S 48.896 69.049 47.791 69.049 z",
+    transform: " matrix(1 0 0 1 0 0) "
+  }), /*#__PURE__*/_react["default"].createElement("path", {
+    d: "M 40.198 43.213 H 33.41 c -1.104 0 -2 -0.896 -2 -2 s 0.896 -2 2 -2 h 6.788 c 1.104 0 2 0.896 2 2 S 41.303 43.213 40.198 43.213 z",
+    transform: " matrix(1 0 0 1 0 0) "
+  }), /*#__PURE__*/_react["default"].createElement("path", {
+    d: "M 56.59 43.213 h -6.788 c -1.104 0 -2 -0.896 -2 -2 s 0.896 -2 2 -2 h 6.788 c 1.104 0 2 0.896 2 2 S 57.694 43.213 56.59 43.213 z",
+    transform: " matrix(1 0 0 1 0 0) "
+  }), /*#__PURE__*/_react["default"].createElement("path", {
+    d: "M 40.198 31.98 H 33.41 c -1.104 0 -2 -0.896 -2 -2 s 0.896 -2 2 -2 h 6.788 c 1.104 0 2 0.896 2 2 S 41.303 31.98 40.198 31.98 z",
+    transform: " matrix(1 0 0 1 0 0) "
+  }), /*#__PURE__*/_react["default"].createElement("path", {
+    d: "M 56.59 31.98 h -6.788 c -1.104 0 -2 -0.896 -2 -2 s 0.896 -2 2 -2 h 6.788 c 1.104 0 2 0.896 2 2 S 57.694 31.98 56.59 31.98 z",
+    transform: " matrix(1 0 0 1 0 0) "
+  }), /*#__PURE__*/_react["default"].createElement("path", {
+    d: "M 40.198 20.748 H 33.41 c -1.104 0 -2 -0.896 -2 -2 s 0.896 -2 2 -2 h 6.788 c 1.104 0 2 0.896 2 2 S 41.303 20.748 40.198 20.748 z",
+    transform: " matrix(1 0 0 1 0 0) "
+  }), /*#__PURE__*/_react["default"].createElement("path", {
+    d: "M 56.59 20.748 h -6.788 c -1.104 0 -2 -0.896 -2 -2 s 0.896 -2 2 -2 h 6.788 c 1.104 0 2 0.896 2 2 S 57.694 20.748 56.59 20.748 z",
+    transform: " matrix(1 0 0 1 0 0) "
+  })));
+};
+var _default = RemoconSVG;
+exports["default"] = _default;
